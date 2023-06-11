2bb7c4841734ff60161212f33fb307ed9efcd54d
ratnesh <ratneshkumar845420@gmail.com>
2023-06-12 02:19:49 +0530

    initial commit

---
+++ Shopaholic-City/src/main/java/com/lpu/shopaholiccity/ShopaholicCityApplication.java
@@ -0,0 +1,13 @@
+package com.lpu.shopaholiccity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopaholicCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopaholicCityApplication.class, args);
+	}
+
+}
